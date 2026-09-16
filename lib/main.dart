4821b803f918c8e50982154a4d664536c35a5116
favour02008 <favourachigati@gmail.com>
2026-09-16 12:45:13 +0000

    vpn fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const FavVPNApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FavVPNApp extends StatelessWidget {
+  const FavVPNApp({super.key});
+    @override
+      Widget build(BuildContext context) {
+          return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                      home: const VPNHome(),
+                          );
+                            }
+                            }
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+                            class VPNHome extends StatefulWidget {
+                              const VPNHome({super.key});
+                                @override
+                                  State<VPNHome> createState() => _VPNHomeState();
+                                  }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+                                  class _VPNHomeState extends State<VPNHome> {
+                                    bool isPremium = false;
+                                      bool isConnected = false;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+                                        void openWhatsApp() async {
+                                            final url = Uri.parse("https://wa.me/233200875946?text=I%20paid%203%20GHS%20for%20VPN%20Premium");
+                                                await launchUrl(url, mode: LaunchMode.externalApplication);
+                                                  }
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
+                                                    @override
+                                                      Widget build(BuildContext context) {
+                                                          return Scaffold(
+                                                                appBar: AppBar(title: const Text("Fav's Ghana VPN"), backgroundColor: Colors.green),
+                                                                      body: Center(
+                                                                              child: Column(
+                                                                                        mainAxisAlignment: MainAxisAlignment.center,
+                                                                                                  children: [
+                                                                                                              Icon(isConnected ? Icons.vpn_lock : Icons.vpn_lock_outlined, size: 100, color: isConnected ? Colors.green : Colors.grey),
+                                                                                                                          const SizedBox(height: 20),
+                                                                                                                                      Text(isConnected ? "Connected" : "Disconnected", style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                                                                                                                                                  const SizedBox(height: 20),
+                                                                                                                                                              ElevatedButton(
+                                                                                                                                                                            onPressed: () => setState(() => isConnected = !isConnected),
+                                                                                                                                                                                          child: Text(isConnected ? "DISCONNECT" : "CONNECT VPN"),
+                                                                                                                                                                                                      ),
+                                                                                                                                                                                                                  const SizedBox(height: 20),
+                                                                                                                                                                                                                              TextButton(onPressed: openWhatsApp, child: const Text("WhatsApp for Code")),
+                                                                                                                                                                                                                                          const SizedBox(height: 10),
+                                                                                                                                                                                                                                                      ElevatedButton(
+                                                                                                                                                                                                                                                                    onPressed: () => setState(() => isPremium = true),
+                                                                                                                                                                                                                                                                                  child: const Text("Get Premium - 3 GHS"),
+                                                                                                                                                                                                                                                                                              ),
+                                                                                                                                                                                                                                                                                                          if (isPremium) const Text("Premium Activated!", style: TextStyle(color: Colors.green)),
+                                                                                                                                                                                                                                                                                                                    ],
+                                                                                                                                                                                                                                                                                                                            ),
+                                                                                                                                                                                                                                                                                                                                  ),
+                                                                                                                                                                                                                                                                                                                                      );
+                                                                                                                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                                                                                                        }
